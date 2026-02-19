8bcb04ac23de31680f5b2895a4359d45ab5f09a5
FenglongSong <fenglongsong1998@gmail.com>
2026-02-19 10:26:13 +0100

    reduce memcpy in update_residual_r()

--- cupiqp/solver.py
+++ cupiqp/solver.py
@@ -626,12 +626,36 @@ class SolverBase:
         It adds the regularization terms to the non-regularized residuals to obtain the regularized residuals.
         """
         # update the rhs of the KKT system
-        self._res.x[:] = self._res_nr.x - self._result.info.rho * (self._result.x - self._prox_vars.x)
-        self._res.y[:] = self._res_nr.y - self._result.info.delta * (self._prox_vars.y - self._result.y)
-        self._res.z_l[:] = self._res_nr.z_l - self._result.info.delta * (self._prox_vars.z_l - self._result.z_l)
-        self._res.z_u[:] = self._res_nr.z_u - self._result.info.delta * (self._prox_vars.z_u - self._result.z_u)
-        self._res.z_bl[:] = self._res_nr.z_bl - self._result.info.delta * (self._prox_vars.z_bl - self._result.z_bl)
-        self._res.z_bu[:] = self._res_nr.z_bu - self._result.info.delta * (self._prox_vars.z_bu - self._result.z_bu)
+        # self._res.x[:] = self._res_nr.x - self._result.info.rho * (self._result.x - self._prox_vars.x)
+        # self._res.y[:] = self._res_nr.y - self._result.info.delta * (self._prox_vars.y - self._result.y)
+        # self._res.z_l[:] = self._res_nr.z_l - self._result.info.delta * (self._prox_vars.z_l - self._result.z_l)
+        # self._res.z_u[:] = self._res_nr.z_u - self._result.info.delta * (self._prox_vars.z_u - self._result.z_u)
+        # self._res.z_bl[:] = self._res_nr.z_bl - self._result.info.delta * (self._prox_vars.z_bl - self._result.z_bl)
+        # self._res.z_bu[:] = self._res_nr.z_bu - self._result.info.delta * (self._prox_vars.z_bu - self._result.z_bu)
+
+        cp.subtract(self._result.x, self._prox_vars.x, out=self._res.x)
+        self._res.x *= self._result.info.rho
+        cp.subtract(self._res_nr.x, self._res.x, out=self._res.x)
+
+        cp.subtract(self._prox_vars.y, self._result.y, out=self._res.y)
+        self._res.y *= self._result.info.delta
+        cp.subtract(self._res_nr.y, self._res.y, out=self._res.y)
+
+        cp.subtract(self._prox_vars.z_l, self._result.z_l, out=self._res.z_l)
+        self._res.z_l *= self._result.info.delta
+        cp.subtract(self._res_nr.z_l, self._res.z_l, out=self._res.z_l)
+
+        cp.subtract(self._prox_vars.z_u, self._result.z_u, out=self._res.z_u)
+        self._res.z_u *= self._result.info.delta
+        cp.subtract(self._res_nr.z_u, self._res.z_u, out=self._res.z_u)
+
+        cp.subtract(self._prox_vars.z_bl, self._result.z_bl, out=self._res.z_bl)
+        self._res.z_bl *= self._result.info.delta
+        cp.subtract(self._res_nr.z_bl, self._res.z_bl, out=self._res.z_bl)
+
+        cp.subtract(self._prox_vars.z_bu, self._result.z_bu, out=self._res.z_bu)
+        self._res.z_bu *= self._result.info.delta
+        cp.subtract(self._res_nr.z_bu, self._res.z_bu, out=self._res.z_bu)
         
         primal_rel_scaling = self._result.info.primal_res / self._result.info.primal_res_rel if self._result.info.primal_res_rel > 0 else 1.
         dual_rel_scaling = self._result.info.dual_res / self._result.info.dual_res_rel if self._result.info.dual_res_rel > 0 else 1.
