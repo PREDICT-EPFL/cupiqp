312d32b5731620910386e1fd062d76777a3b354f
FenglongSong <fenglongsong1998@gmail.com>
2026-06-01 09:01:26 +0200

    remove the jax and numpy implementation of piqp which are no longer used

--- piqp_jax/typedef.py
+++
@@ -1,7 +0,0 @@
-import jax
-
-Vector = jax.Array  # 1D array
-Matrix = jax.Array  # 2D array
-
-
-PIQP_INF = 1e20
--- piqp_numpy/typedef.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-Vector = np.ndarray  # 1D array
-Matrix = np.ndarray  # 2D array
-
-PIQP_INF = 1e20
